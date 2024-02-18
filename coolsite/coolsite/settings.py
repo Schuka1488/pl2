a32e3ee6d7e2683d3ec89c840a5499ddab1b52c1
Максим Щукин <oleg-14317@mail.ru>
2024-02-18 14:37:24 +0500

    Shipment #19

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -9,9 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aa_ds0r0=1wzt%#tyhb30a)2lk4h=ighhyg2xs^&27fo4*$i27'
+SECRET_KEY = 'django-insecure-aa_ds0r0=1wzt%#tyhb30a)2lk4h=ighhyy2xs^&27fo4*$i27'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
